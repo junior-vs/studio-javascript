a58f8e6c3ad659a8dbb6b3b1d81d506c33752144
Valdir Junior <junior_vs@outlook.com>
2019-10-01 16:34:03 -0300

    aula 4 e  5

---
+++ javascript-avancado-p1/aluraframe/client/js/app/view/NegociacoesView.js
@@ -0,0 +1,20 @@
+class NegociacoesView {
+
+    template() {
+
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+     </table>
+            `;
+    }
+}
